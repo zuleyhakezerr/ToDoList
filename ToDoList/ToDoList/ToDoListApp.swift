cdd86bef59d82d5b27f4ed3fb9062dc0c5413cc2
zuleyha <zuleyhakezerr@gmail.com>
2024-03-29 11:12:05 +0300

    Add Project Base

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Züleyha on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
